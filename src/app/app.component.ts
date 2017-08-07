c64d55cb885b8de769b4fb7856732bdb21b3d778
oomusou <oomusou@gmail.com>
2017-08-07 11:48:16 +0800

    單元測試 (綠燈)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  selectedId = '0';
+
+  onChange(element: HTMLSelectElement) {
+    this.selectedId = element.value;
+  }
 }
